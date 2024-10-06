c8ef989474691f08426fd17d4855385e3fbb7049
Omar Sherif <omarsheriff5656@gmail.com>
2024-10-06 17:38:39 +0300

    swagger

--- index.js
+++ index.js
@@ -1,25 +1,98 @@
 const express = require('express');
 
-const mongoose = require('mongoose');
+const swagger_ui= require('swagger-ui-express');
 
-const prodRouter = require('../lab 2/ProductRouter')
+const yaml = require('yamljs')
+
+const path = require('path')
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+const swaggerDoc = yaml.load(path.join(__dirname, 'swagger.yaml'));
 
-const url = 'mongodb://localhost:27017/Shopping';
+app.use('/api-docs', swagger_ui.serve, swagger_ui.setup(swaggerDoc));
 
-mongoose.connect(url, {
+let students = [];
 
-    useNewUrlParser: true, 
+app.get('/api/v1/student/:id', (req, res) => {
 
-    //useUnifiedTopolgy: true
+    const id = req.params.id;
 
-}).then(console.log('Connected To the DB')).catch(err => console.log(err.message));
+    for(let student of students){
 
-app.use('/products', prodRouter);
+        if(student.id == id){
+
+            return res.status(200).json(student);
+
+        }
+    }
+
+    return res.status(404).json('No such student');
+    
+})
+
+app.post('/api/v1/student', (req, res) => {
+
+    const {name, id, major, studyYear, gpa} = req.body;
+
+    if(!(name && id && major && studyYear && gpa)){
+
+        return res.status(401).json('Fill in all the fields');
+
+    }
+
+    const student = {id, name, major, studyYear, gpa};
+
+    students.push(student);
+
+    return res.status(201).json(student);
+})
+
+app.delete('/api/v1/student/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    for(let i = 0;i < students.length;i++){
+        
+        if(students[i].id === id){
+
+            students.splice(i , 1);
+
+            return res.status(200).json('Deleted Successfully');
+
+        }
+    }
+
+    return res.status(404).json('No such Student');
+})
+
+app.put('/api/v1/student', (req, res) => {
+
+    const {name, id, major, studyYear, gpa} = req.body;
+
+    if(!(name && id && major && studyYear && gpa)){
+
+        return res.status(401).json('Fill in all the fields');
+
+    }
+
+    const student = {name, major, studyYear, gpa};
+
+    for(let i = 0;i < students.length;i++){
+
+        if(i.id === id){
+
+            students[i] = student;
+
+            return res.status(200).json(student);
+
+        }
+    }
+
+    return res.status(404).json('No such student');
+})
+
 
 app.listen(3000);
